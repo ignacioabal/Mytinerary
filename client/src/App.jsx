5592b0a36e646e22e9953afd56d9cc10708488f7
Ignacio Abal <ignacioleonardoabal@gmail.com>
2019-11-28 14:44:05 -0300

    added a city header for available itineraries

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,29 @@
-import React from 'react';
-import Header from './components/header/Header';
-import Home from './components/home/Home';
-import Login from './components/users/Login';
-import Register from './components/users/Register';
-import Cities from './components/cities/Cities'
-import { BrowserRouter, Route } from 'react-router-dom';
-import './components/util/LandingPage.css';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/js/dist/dropdown'; import 'bootstrap/js/dist/carousel';
-import AvItineraries from './components/itineraries/AvailableItineraries';
-
-
+import React from "react";
+import Home from "./components/home/Home";
+import Login from "./components/users/Login";
+import Register from "./components/users/Register";
+import Cities from "./components/cities/Cities";
+import { BrowserRouter, Route } from "react-router-dom";
+import "./components/util/LandingPage.css";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/js/dist/dropdown";
+import "bootstrap/js/dist/carousel";
+import AvItineraries from "./components/itineraries/AvailableItineraries";
 
 class App extends React.Component {
-    render() {
-        return (
-            <BrowserRouter>
-                <div className="App container">
-                    <Header />
-
-                    <Route path="/" exact component={Home} />
-                    <Route path="/login" exact component={Login} />
-                    <Route path="/register" exact component={Register} />
-                    <Route path="/cities" exact component={Cities} />
-                    <Route path="/cities/barcelona" exact component={AvItineraries}/>
-
-                </div>
-            </BrowserRouter>
-        );
-    }
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="App container">
+          <Route path="/" exact component={Home} />
+          <Route path="/login" exact component={Login} />
+          <Route path="/register" exact component={Register} />
+          <Route path="/cities" exact component={Cities} />
+          <Route path="/cities/barcelona" exact component={AvItineraries} />
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
--- client/src/components/itineraries/AvailableItineraries.jsx
+++ client/src/components/itineraries/AvailableItineraries.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 import Itinerary from "./Itinerary";
+import Header from "./CityHeader";
 
 export default class AvItineraries extends Component {
   render() {
     return (
       <React.Fragment>
+        <Header></Header>
         <Itinerary></Itinerary>
       </React.Fragment>
     );
