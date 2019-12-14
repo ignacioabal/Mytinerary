8768101421c62f36d9a2960e15bcb900e953dbf4
Ignacio Abal <ignacioleonardoabal@gmail.com>
2019-12-14 08:36:24 -0300

    It kinda works

--- client/src/components/itineraries/ActivitiesCarousel.jsx
+++ client/src/components/itineraries/ActivitiesCarousel.jsx
@@ -2,35 +2,36 @@ import React, { useState, Component } from "react";
 import ItemsCarousel from "react-items-carousel";
 import "./Carousel.css";
 
-export default class Carousel extends Component {
-  getActivities() {
-    let acts = this.props.acts;
+export default function Carousel() {
+  // getActivities() {
+  //   let acts = this.props.acts;
 
-    acts.map(acts => {
-      return <div className="carItem">{acts}</div>;
-    });
-  }
+  //   acts.map(acts => {
+  //     return <div className="carItem">{acts}</div>;
+  //   });
+  // }
 
-  render() {
-    const [activeItemIndex, setActiveItemIndex] = useState(0);
-    const chevronWidth = 40;
-
-    return (
-      <div id="actCar">
-        <ItemsCarousel
-          requestToChangeActive={setActiveItemIndex}
-          activeItemIndex={activeItemIndex}
-          numberOfCards={2}
-          gutter={20}
-          rightChevron={<i className="fas fa-chevron-right"></i>}
-          leftChevron={<i className="fas fa-chevron-left"></i>}
-          outsideChevron
-          chevronWidth={chevronWidth}
-          infiniteLoop={true}
-        >
-          {this.getActivities}
-        </ItemsCarousel>
-      </div>
-    );
-  }
+  // render() {
+  const [activeItemIndex, setActiveItemIndex] = useState(0);
+  const chevronWidth = 40;
+  return (
+    <div id="actCar">
+      <ItemsCarousel
+        requestToChangeActive={setActiveItemIndex}
+        activeItemIndex={activeItemIndex}
+        numberOfCards={2}
+        gutter={20}
+        rightChevron={<i className="fas fa-chevron-right"></i>}
+        leftChevron={<i className="fas fa-chevron-left"></i>}
+        outsideChevron
+        chevronWidth={chevronWidth}
+        infiniteLoop={true}
+      >
+        <div className="carItem">Activity 1</div>
+        <div className="carItem">Activity 2</div>
+        <div className="carItem">Activity 3</div>
+      </ItemsCarousel>
+    </div>
+  );
+  // }
 }
